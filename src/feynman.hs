b94f7faf89ac6923d0ce2624515ab0caf4e1070d
Jesse Straat <jessestraat1@gmail.com>
2022-11-16 22:29:05 +0100

    Removed permutation symmetries

--- src/feynman.hs
+++ src/feynman.hs
@@ -3,11 +3,13 @@ import Data.List (splitAt)
 data Object = Source String | Vertex String deriving (Show)
 -- Syntax: Source {name} or Vertex {name}
 instance Eq Object where
-    Source x == Source y = x == y
+    Source x == Source y = True
     Vertex x == Vertex y = x == y
     Source x == Vertex y = False
 
 data Graph = Graph [(Object, Object)] deriving (Show)
+instance Eq Graph where
+    Graph xs == Graph ys = xs `elem` (permutations ys)
 
 isInt :: (RealFrac a) => a  -> Bool
 isInt x = x == fromInteger (round x)
@@ -20,15 +22,17 @@ graphGenerate olist n
     | otherwise           = [(olist !! 0, olist !! i) : list | i <- [1..(length olist - 1)], list <- (graphGenerate (pop (pop olist i) 0) (n-1))]
     where (x:xs) = olist
 
-feynmanGenerate :: Int -> Int -> Int -> [[(Object, Object)]]
+feynmanGenerate :: Int -> Int -> Int -> [Graph]
 -- E -> V -> # of legs -> [Feynman diagram]
 feynmanGenerate e v legs
-    | isInt p   = rmdups(graphGenerate olist (round p))
+    -- Complexity O(n!n^2) with n = length graphs
+    | isInt p   = rmdups [Graph g | g <- graphs]
     | otherwise = []
     where elist = [Source (show x) | x <- [1..e]]
           vlist = [Vertex (show x) | x <- [1..v]]
           olist = elist ++ (replicateList vlist legs)
           p = (fromIntegral (e + legs*v))/2
+          graphs = graphGenerate olist (round p)
 
 replicateList :: [a] -> Int -> [a]
 replicateList xs n = xs >>= replicate n
@@ -41,7 +45,13 @@ pop xs n
     where (ys, z:zs) = splitAt n xs
 
 rmdups :: (Eq a) => [a] -> [a]
+-- Complexity O(n^2)
 rmdups [] = []
 rmdups (x:xs)
     | x `elem` xs = rmdups xs
     | otherwise   = x : rmdups xs
+
+permutations :: [a] -> [[a]]
+-- Complexity O(n!)
+permutations [] = [[]]
+permutations xs = [(xs !! i) : list | i <- [0..(length xs - 1)], list <- permutations (pop xs i)]
