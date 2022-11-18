3ac17fcdca3d9971cc194b1e68c8a501d9c3ea46
Jesse Straat <jessestraat1@gmail.com>
2022-11-18 16:48:14 +0100

    Edited isPermutation to output Bool, rather than IO(Bool), using Hashmaps

--- src/Ispermutation.hs
+++ src/Ispermutation.hs
@@ -3,40 +3,27 @@ module Ispermutation
     isPermutation
 ) where
 
-import qualified Data.HashTable.IO as H
+import qualified Data.HashMap.Strict as H (HashMap, empty, lookup, insert, delete, null)
 import Data.Hashable (Hashable)
 import Data.Maybe (fromMaybe)
 
-type HashTable k v = H.BasicHashTable k v
+histogram :: (Hashable a) => [a] -> H.HashMap a Int
+histogram [] = H.empty
+histogram (x:xs) = H.insert x (val + 1) hist
+    where hist = histogram xs
+          val = fromMaybe 0 (H.lookup x hist)
 
-histogram :: (Hashable a) => [a] -> IO(HashTable a Int)
-histogram [] = do
-    ht <- H.new
-    return ht
-histogram (x:xs) = do
-    ht <- histogram xs
-    val <- H.lookup ht x
-    H.insert ht x (fromMaybe 0 val + 1)
-    return ht
+compareHistogram :: (Hashable a) => [a] -> H.HashMap a Int -> Bool
+compareHistogram [] hist
+    | H.null hist = True
+    | otherwise   = False
+compareHistogram (x:xs) hist
+    | xfreq == 0 = False
+    | otherwise  = compareHistogram xs newHist
+    where xfreq = fromMaybe 0 (H.lookup x hist)
+          newHist
+            | xfreq == 1 = H.delete x hist
+            | otherwise  = H.insert x (xfreq - 1) hist
 
-compareHistogram :: (Hashable a) => [a] -> IO(HashTable a Int) -> IO(Bool)
-compareHistogram [] hio = do
-    ht <- hio
-    hlist <- H.toList ht
-    if hlist == []
-        then return True
-        else return False
-compareHistogram (x:xs) hio = do
-    ht <- hio
-    xval <- H.lookup ht x
-    let xfreq = fromMaybe 0 xval
-    if xfreq == 0
-        then return False
-        else do
-            if xfreq == 1
-                then H.delete ht x
-                else H.insert ht x (xfreq - 1)
-            compareHistogram xs (return ht)
-
-isPermutation :: (Hashable a) => [a] -> [a] -> IO(Bool)
+isPermutation :: (Hashable a) => [a] -> [a] -> Bool
 isPermutation xs ys = compareHistogram xs (histogram ys)
